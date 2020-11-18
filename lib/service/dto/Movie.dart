2bf9aa5c6da075a44b5575eacff1f96ea5847af5
Zhu Mei <meizhu812@outlook.com>
2020-11-18 14:41:53 +0800

    Add Movie dto.

---
+++ lib/service/dto/Movie.dart
@@ -0,0 +1,12 @@
+class Movie {
+  final String title;
+  final String year;
+  final String posterUrl;
+
+  Movie({this.title, this.year, this.posterUrl});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+        title: json["Title"], year: json["Year"], posterUrl: json["Poster"]);
+  }
+}

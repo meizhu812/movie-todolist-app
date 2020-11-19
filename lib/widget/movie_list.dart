285b5cb32b7b3775c7d2b39665343be8f25a44bf
Zhu Mei <meizhu812@outlook.com>
2020-11-19 16:53:07 +0800

    changed some styles

--- lib/widget/movie_list.dart
+++ lib/widget/movie_list.dart
@@ -12,22 +12,46 @@ class MovieList extends StatelessWidget {
       itemCount: this.movies.length,
       itemBuilder: (context, index) {
         final movie = this.movies[index];
-
-        return ListTile(
-          contentPadding: EdgeInsets.all(10),
-          leading: Container(
+        return Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(movie.posterUrl),
+              border: Border(
+                bottom: BorderSide(color: Color(0xFFDDDDDD), width: 1.0),
               ),
-              borderRadius: BorderRadius.circular(6),
             ),
-            width: 50,
-            height: 100,
-          ),
-          title: Text(movie.title),
-        );
+            child: ListTile(
+              contentPadding: EdgeInsets.all(10),
+              leading: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(movie.posterUrl),
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                width: 50,
+                height: 100,
+              ),
+              title: Text(
+                movie.title,
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Text(
+                movie.year,
+                style: TextStyle(
+                  fontSize: 12.0,
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Color(0XFFFF0000),
+                ),
+                onPressed: () {},
+              ),
+            ));
       },
     );
   }

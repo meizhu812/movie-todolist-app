a405c76e5fbe38dc0f6cea4c3ddc96a97d063c2c
Zhu Mei <meizhu812@outlook.com>
2020-11-20 10:00:55 +0800

    add imdbId field for movie model

--- lib/service/model/movie.dart
+++ lib/service/model/movie.dart
@@ -1,14 +1,16 @@
 class Movie {
   final String title;
   final String year;
+  final String imdbId;
   final String posterUrl;
 
-  Movie({this.title, this.year, this.posterUrl});
+  Movie({this.title, this.year, this.imdbId, this.posterUrl});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
       title: json["Title"],
       year: json["Year"],
+      imdbId: json["imdbID"],
       posterUrl: json["Poster"],
     );
   }
